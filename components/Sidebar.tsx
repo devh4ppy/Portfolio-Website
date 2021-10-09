70caf115c7c4d780755346ddbdc024ebcc67b9ea
devh4ppy <91412628+devh4ppy@users.noreply.github.com>
2021-10-10 03:36:49 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -38,7 +38,7 @@ const Sidebar = () => {
                 <a href="https://github.com/devh4ppy">
                     <AiFillGithub className=" w-8 h-8 cursor-pointer " />
                 </a>
-                <a href="https://www.linkedin.com/in/pankaj-singh-982110219">
+                <a href="www.linkedin.com/in/pankaj-singh-691a6a222">
                     <AiFillLinkedin className=" w-8 h-8 cursor-pointer" />
                 </a>
                 <a href="">
