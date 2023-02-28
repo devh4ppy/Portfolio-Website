0dbe1cd20c45f9ac9cb099841c9ec8f5bc672628
Pankaj Singh <91412628+devh4ppy@users.noreply.github.com>
2023-02-28 18:49:41 +0530

    Update resume.tsx

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -9,7 +9,14 @@ const resume = () => {
     return (
         <div className="px-6 py-2">
             {/* education */}
-            <div className='grid-rows-1 text-center text-2xl font-bold '><h5>Education</h5></div>
+            <div className='grid-md:grid-cols-2 text-center text-2xl font-bold '>
+            <div>
+            <h5>Education</h5>
+            </div>
+            <div>
+            <h5>Experience</h5>
+            </div>
+            </div>
             {/* education info */}
             <div className='grid gap-6 md:grid-cols-2 my-3 '>
                 <motion.div variants={fadeInUp} initial="initial" animate="animate">
@@ -18,13 +25,14 @@ const resume = () => {
                     <p>Skn Sinhgad College Of Engineering</p>
                     <p>2018-2021</p>
                     <p className="my-3 font-semibold">CGPA: 7.41/10</p>
-                </motion.div>
-                <motion.div variants={fadeInUp} initial="initial" animate="animate">
                     <h5 className='my-2 text-xl font-bold'>Diploma</h5>
                     <p className='font-semibold '>Computer Technology</p>
                     <p>Government Polytechnic Pen</p>
                     <p>2016-2018</p>
                     <p className="my-3 font-semibold">Aggregate Percentage: 62.39%</p>
+                </motion.div>
+                <motion.div variants={fadeInUp} initial="initial" animate="animate">
+                    <h5 className='my-2 text-xl font-bold'>Faclon Labs</h5>
                 </motion.div>
             </div>
             {/* languages & tools */}
