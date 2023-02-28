8d9eaccf4bc870bd8b3a62822f1555c2fc616c21
Pankaj Singh <91412628+devh4ppy@users.noreply.github.com>
2023-02-28 18:30:12 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,7 @@ const index = () => {
   return (
     <div className="flex flex-col px-6 pt-1 flex-grow">
       <title>Prohappy2.1 </title>
-      <h5 className="my-3 font-medium">I'm a backend developer with a passion for creating efficient, reliable, and secure web applications. I specialize in developing REST APIs, database systems, and server-side scripting. I have a deep understanding of software engineering principles, algorithms, and design patterns, and I'm always learning new technologies to stay ahead of the curve.
-      </h5>
+      <h5 className="my-3 font-medium">I'm a backend developer with a passion for creating efficient, reliable, and secure web applications. I specialize in developing REST APIs, database systems, and server-side scripting. I have a deep understanding of software engineering principles, algorithms, and design patterns, and I'm always learning new technologies to stay ahead of the curve.</h5>
       <div className="p-4 mt-5 bg-gray-200 dark:bg-dark-100 flex-grow"
       style={{ marginLeft: '-1.5rem', marginRight: '-1.5rem'}}>
           <h6 className="my-3 text-xl font-bold tracking-wide">What I am doing</h6>
