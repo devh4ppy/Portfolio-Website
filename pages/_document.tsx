4206e7dea5940cd1701fbcda8e1478712930bee7
Pankaj Singh <91412628+devh4ppy@users.noreply.github.com>
2023-02-28 23:20:33 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
-        <body className="bg-fixed h-72 bg-gradient-to-b from-purple-600 to-blue-600 dark:from-dark-500 dark:to-dark-700 dark:text-white">
+        <body className="bg-fixed linear-gradient(to right, rgb(255, 228, 230), rgb(204, 251, 241)) dark:from-dark-500 dark:to-dark-700 dark:text-white">
           <Main />
           <NextScript />
         </body>
